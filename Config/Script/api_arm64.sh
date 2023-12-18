ae43e8e9f6fbf9e57a450d88136a4590f6a97ff8
3wking <1094890624@qq.com>
2023-12-19 01:29:37 +0800

    创建 api_arm64.sh

---
+++ Config/Script/api_arm64.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+dir="/usr/share/api/"
+file="api_arm"
+api="https://github.cooluc.com/https://raw.githubusercontent.com/3wking/OpenWrt/main/Config/api_arm"
+url="https://github.cooluc.com/https://raw.githubusercontent.com/3wking/OpenWrt/main/Config/Config/"
+if [ ! -e $dir$file ]; then
+	mkdir -p $dir
+	wget $api -O $dir$file
+	chmod +x $dir$file
+fi	
+	cd $dir && ./$file $pwd $url
