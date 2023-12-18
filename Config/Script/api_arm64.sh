e711dfe82c6f6649df37ea9541e207b7bf8d8926
3wking <1094890624@qq.com>
2023-12-19 01:33:49 +0800

    UP

--- Config/Script/api_arm64.sh
+++ Config/Script/api_arm64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dir="/usr/share/api/"
 file="api_arm64"
-api="https://github.cooluc.com/https://raw.githubusercontent.com/3wking/OpenWrt/main/Config/api_arm64"
+api="https://github.cooluc.com/https://raw.githubusercontent.com/3wking/OpenWrt/main/Config/API/api_arm64"
 url="https://github.cooluc.com/https://raw.githubusercontent.com/3wking/OpenWrt/main/Config/Config/"
 if [ ! -e $dir$file ]; then
 	mkdir -p $dir
